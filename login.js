8d65a7165c362e9d86c56eaf0a049b15eb5c7b60
Mihir Patel <mihirkpatel8829@gmail.com>
2023-03-11 17:02:28 -0500

    new files added

---
+++ login.js
@@ -0,0 +1,49 @@
+// import wixUsersBackend from 'wix-users-backend';
+
+// export function myBackendFunction(email, password) {
+//   return wixUsersBackend.login(email, password)
+//     .then( (sessionToken) => {
+//       return {sessionToken, "approved": true};
+//     } )
+//     .catch( (error) => {
+//       return {"approved": false, "reason": error};
+//     } );
+// }
+
+const users = [
+	{email: 'user1@gmail.com', password: '123'},
+	{email: 'user2@gmail.com', password: '234'},
+	{email: 'user3@gmail.com', password: '345'}
+];
+
+const loginBtn = document.getElementById('login-btn');
+const signupLink = document.getElementById('signup-link');
+
+loginBtn.addEventListener('click', function(event) {
+	event.preventDefault(); // Prevent form submission
+
+	// Get user input values
+	const email = document.getElementById('email').value;
+	const password = document.getElementById('password').value;
+
+	// Check if user exists
+	const userExists = users.some(function(user) {
+		return user.email === email && user.password === password;
+	});
+
+	if (userExists) {
+		alert('Login successful!');
+		// Redirect to dashboard or home page
+	} else {
+		const signup = confirm('User not found. Do you want to sign up?');
+		if (signup) {
+			// Redirect to sign up page
+			window.location.href = 'signup.html';
+		}
+	}
+});
+
+signupLink.addEventListener('click', function(event) {
+	event.preventDefault(); // Prevent link click behavior
+	window.location.href = 'signup.html'; // Redirect to sign up page
+});
---
+++ signup.js
@@ -0,0 +1,48 @@
+const users = [
+	{email: 'newuser1@gmail.com', password: '123'},
+	{email: 'newuser2@gmail.com', password: '234'}
+];
+
+const signupBtn = document.getElementById('signup-btn');
+const passwordModal = document.getElementById('password-modal');
+const closeBtn = document.querySelector('.close');
+const createBtn = document.getElementById('create-btn');
+
+signupBtn.addEventListener('click', function(event) {
+	event.preventDefault(); // Prevent form submission
+
+	// Get user input value
+	const email = document.getElementById('email').value;
+
+	// Check if user exists
+	const userExists = users.some(function(user) {
+		return user.email === email;
+	});
+
+	if (userExists) {
+		alert('User already exists. Please log in.');
+		// Redirect to login page
+	} else {
+		// Open password prompt modal
+		passwordModal.style.display = 'block';
+	}
+});
+
+closeBtn.addEventListener('click', function() {
+	// Close password prompt modal
+	passwordModal.style.display = 'none';
+});
+
+createBtn.addEventListener('click', function(event) {
+	event.preventDefault(); // Prevent form submission
+
+	// Get user input value
+	const email = document.getElementById('email').value;
+	const password = document.getElementById('password').value;
+
+	// Add new user
+	users.push({email, password});
+
+	alert('Account created successfully!');
+	// Redirect to login page
+});
